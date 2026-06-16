8e2ff81ea4e87ed5fdeffacfb516f94938f2e1ed
ZIUGOD <caique.dubstep@gmail.com>
2026-06-16 20:07:39 -0300

    New: suggesting to reboot

--- debian_update.sh
+++ debian_update.sh
@@ -17,4 +17,10 @@ fastfetch
 
 if [ -f /var/run/reboot-required ]; then
     echo ">> [WARNING] A reboot is required to apply kernel updates."
+    read -r -p ">> Do you want to reboot now? (y/N) " response
+    if [[ "$response" =~ ^[Yy]$ ]]; then
+        sudo reboot
+    else
+        echo ">> Please remember to reboot as soon as possible to ensure your system is secure and up-to-date."
+    fi
 fi
